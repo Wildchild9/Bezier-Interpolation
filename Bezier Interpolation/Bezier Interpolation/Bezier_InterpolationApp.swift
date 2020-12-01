81c474d5d26662403d2146b204364419a6239abc
Noah Wilder <noahleviwilder@gmail.com>
2020-12-01 17:05:09 -0500

    Created Xcode project.

---
+++ Bezier Interpolation/Bezier Interpolation/Bezier_InterpolationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bezier_InterpolationApp.swift
+//  Bezier Interpolation
+//
+//  Created by Noah Wilder on 2020-12-01.
+//
+
+import SwiftUI
+
+@main
+struct Bezier_InterpolationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
